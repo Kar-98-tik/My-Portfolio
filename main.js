52f2cb95caccd99ede644dd94888417113331fca
Kartik Prayagi <142045618+Kar-98-tik@users.noreply.github.com>
2024-01-01 22:42:41 +0530

    added main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert('Project Coming Soon!');
+}
+
+var typed = new Typed('#element', {
+    strings: ['Software Development,', 'Web Development,', 'AI&amp;ML.'],
+    typeSpeed: 50,
+});
